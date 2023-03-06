2577620d9b122e1bded460bea21475cc737bd43a
user1 <user1@gmail.com>
2023-03-06 17:19:27 +0000

    3rd code commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//document.write("Ankush");
+//console.log("Hello World");
+/*
+var a;
+a = 3;
+alert(a);
+
+/*this
+is 
+comment*/
+
+// container = variable
+// var is called variable
+// we store the data in variable
+
+// example
+// var digit1 = 33;
+// we declared a variable name "digit1" and 33 stored in it
+
+// alert(digit1);
+
+// after ES6, we use 'var' as 'let' and 'const'
+/*
+let digit = 22;
+
+const door = 33;
+
+digit = 11; //let variable data can change or redefine
+// door = 12; //but constant value cannot be change
+
+console.log(digit);
+console.log(door);
